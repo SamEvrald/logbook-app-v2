aecbffcdb6e4e43b6b05387c848912d1b7fec57b
SamEvrald <msamevrald@gmail.com>
2025-04-22 13:31:07 +0200

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,13 +1,23 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "sam",
-  password: "Nexa721Li",
-  database: "logbook_db",
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 0
+});
+
+db.on('acquire', function (connection) {
+  console.log('📥 Connection %d acquired', connection.threadId);
+});
+
+db.on('release', function (connection) {
+  console.log('📤 Connection %d released', connection.threadId);
 });
 
 module.exports = db;
+
+
