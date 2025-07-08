06d4bd9fb5b9227193dc55dd5c73d74494ecd12e
SamEvrald <msamevrald@gmail.com>
2025-07-08 13:58:26 +0200

    Update AdminLoginPage.js

--- frontend/logbook-web/src/pages/AdminLoginPage.js
+++ frontend/logbook-web/src/pages/AdminLoginPage.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import API from "../api/api";
 import { useNavigate } from "react-router-dom";
-import Footer from "../components/Footer"; // ✅ Correctly import Footer component
-import TopBar from "../components/Shared/TopBar"; // ✅ Import TopBar
+import Footer from "../components/Footer"; 
+import TopBar from "../components/Shared/TopBar"; 
 
 const AdminLoginPage = () => {
   const [email, setEmail] = useState("");
@@ -27,7 +27,7 @@ const AdminLoginPage = () => {
 
   return (<div style={{ maxWidth: "400px", margin: "50px auto" }}>
     
-      <TopBar /> {/* ✅ Add TopBar at the Top */}
+      <TopBar /> 
       <h2>Admin Login</h2>
       <form onSubmit={handleLogin}>
         {error && <p style={{ color: "red" }}>{error}</p>}
@@ -41,7 +41,7 @@ const AdminLoginPage = () => {
         </div>
         <button type="submit">Login</button>
       </form>
-      {/* ✅ Correct Footer Placement */}
+      
       <Footer />
     </div>
   );
