c840491abf0d2e74103d8c4a7d8aca828c71d1bf
SamEvrald <msamevrald@gmail.com>
2025-07-08 12:10:33 +0200

    Update studentRoutes.js

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getStudentCourses } = require("../controllers/studentController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-// ✅ Fetch student courses (Ensure moodle_instance_id is passed)
+//  Fetch student courses (Ensure moodle_instance_id is passed)
 router.get("/courses", authMiddleware, getStudentCourses);
 
 module.exports = router;
